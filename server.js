f8fe1d9019f3b3e31663ef191f55a50b0a41fef0
Placinta Alex <alexplacinta94@mail.ru>
2015-05-26 14:43:15 +0300

    asd

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 var express = require('express');
 
 var app = express.createServer();
-app.listen(8080, '127.0.0.1', function(){console.log('Server run on port 8080 ip adress 127.0.0.1')});
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.listen(port, ipaddress, function(){console.log('Server run on port 8080 ip adress 127.0.0.1')});
+
+//routes
 app.get('/', function(req, res){
     res.send('<h2 style="color: red;">Hello world!!!!</h2>');
 });
