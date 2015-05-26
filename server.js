61deb98550a7c06c69cc4a5c3740b49392d9b59f
Placinta Alex <alexplacinta94@mail.ru>
2015-05-26 15:36:56 +0300

    dsa

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 
 var express = require('express');
-
-var app = express.createServer();
+var mongodb = require('mongodb');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-app.listen(port, ipaddress, function(){console.log('Server run on port 8080 ip adress 127.0.0.1')});
+
+var MongoClient = mongodb.MongoClient;
+
+
+var connection_string = '127.0.0.1:27017/recommendfilm';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+
+
+
+var app = express.createServer();
+
+
+app.listen(port, ipaddress, function(){console.log('Server runs...')});
 
 //routes
 app.get('/', function(req, res){
     res.send('<h2 style="color: red;">Hello world!!!!</h2>');
+});
+
+app.get('/users', function(req, res){
+    MongoClient.connect('mongodb://'+connection_string, function (err, db) {
+        if (err) {
+            res.send('error: '+err);
+        } else {
+            var collection = db.collection('users').find().limit(10).toArray(function(err, docs) {
+                res.send(JSON.stringify(collection));
+                db.close();
+            });
+        }
+    });
 });
 
 
