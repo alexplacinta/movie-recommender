90f3e77acb15e7ec9869ba65b7623ed05a01ca5f
Placinta Alex <alexplacinta94@mail.ru>
2015-05-30 18:23:42 +0300

    dsadsa

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/recommendedmovies', function(req, res){
 
 app.post('/recommendedmovies', function(req, res){
 
-    var data = {movie: req.get('movie'), comment: req.get('comment')};
+    var data = {movie: req.body.movie, comment: req.body.comment};
 
     MongoClient.connect('mongodb://'+connection_string, function (err, db) {
         if (err) {
