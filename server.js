d32bf1a1628058fa15005315fd3766b6cac45524
Placinta Alex <alexplacinta94@mail.ru>
2015-05-31 03:16:21 +0300

    das

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.get('/recommendedmovies', function(req, res){
 });
 
 app.post('/recommendedmovies', function(req, res){
-    res.send(req.body.movie);
     var data = {movie: req.body.movie, comment: req.body.comment};
 
     MongoClient.connect('mongodb://'+connection_string, function (err, db) {
